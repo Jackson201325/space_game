fadcde3aaeeffcf998ddab0b2b9fc54cb0e3fc66
Jackson Huang <jacksonh201325@gmail.com>
2023-11-24 13:23:47 +0100

    Added bullet collision

--- game_functions.py
+++ game_functions.py
@@ -42,13 +42,13 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ai_settings, screen, ship, bullets):
-    if event.key == pygame.K_RIGHT:
+    if event.key == pygame.K_RIGHT or event.key == pygame.K_l:
         ship.moving_right = False
-    elif event.key == pygame.K_LEFT:
+    elif event.key == pygame.K_LEFT or event.key == pygame.K_h:
         ship.moving_left = False
 
 
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Update position of bullets and get rid of old bullets."""
     # Update bullet positions.
     bullets.update()
@@ -56,6 +56,13 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    # repopulate fleet
+    if len(aliens) == 0:
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet if limit not reached yet."""
@@ -97,3 +104,24 @@ def get_number_rows(ai_settings, ship_height, alien_height):
         (3 * alien_height) - ship_height)
     number_rows = int(available_space_y / (2 * alien_height))
     return number_rows
+
+def check_fleet_edges(ai_settings, aliens):
+    """Respond appropriately if any aliens have reached an edge."""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    """Drop the entire fleet and change the fleet's direction."""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+def update_aliens(ai_settings, aliens):
+    """
+    Check if the fleet is at an edge,
+        then update the positions of all aliens in the fleet.
+    """
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
--- settings.py
+++ settings.py
@@ -11,8 +11,15 @@ class Settings():
         self.ship_speed_factor = 1.9
 
         # Bullet settings
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_speed_factor = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60, 60 ,60
         self.bulltets_allowed = 69
+
+        self.fleet_drop_speed = 5
+
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+        self.alien_speed_factor = 0.5
