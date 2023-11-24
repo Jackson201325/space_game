b5f9ad0ee0ac6c67de31552df597b0c46ca558de
Jackson Huang <jacksonh201325@gmail.com>
2023-11-24 13:04:51 +0100

    Added aliens

--- alien_invation.py
+++ alien_invation.py
@@ -2,7 +2,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-from alien import Alien
+# from alien import Alien
 from pygame.sprite import Group
 
 import game_functions as gf
@@ -16,14 +16,18 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
 
     ship = Ship(ai_settings, screen)
-    alien = Alien(ai_settings, screen)
+
+    # Group of ships and aliens
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets, alien)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events."""
@@ -14,15 +15,16 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ai_settings, screen, ship, bullets)
 
-        elif event.key == pygame.K_q:
+        elif event.type == pygame.K_q:
             sys.exit()
 
 
-def update_screen(ai_settings, screen, ship, bullets, alien):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Update images on the screen and flip to the new screen."""
     screen.fill(ai_settings.bg_color)
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
+    # alien.blitme()
 
     for bullet in bullets.sprites():
         bullet.draw_bullet()
@@ -32,9 +34,9 @@ def update_screen(ai_settings, screen, ship, bullets, alien):
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """RESPOND TO KEY PRESSES."""
-    if event.key == pygame.K_RIGHT:
+    if event.key == pygame.K_RIGHT or event.key == pygame.K_l:
         ship.moving_right = True
-    elif event.key == pygame.K_LEFT:
+    elif event.key == pygame.K_LEFT or event.key == pygame.K_h:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
@@ -61,3 +63,37 @@ def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bulltets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
+
+def create_fleet(ai_settings, screen, ship, aliens):
+    """Create a full fleet of aliens."""
+    # Create an alien and find the number of aliens in a row.
+    alien = Alien(ai_settings, screen)
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height,
+        alien.rect.height)
+
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, screen, aliens, alien_number, row_number)
+
+def get_number_aliens_x(ai_settings, alien_width):
+    """Determine the number of aliens that fit in a row."""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
+    """Create an alien and place it in the row."""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+    aliens.add(alien)
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    """Determine the number of rows of aliens that fit on the screen."""
+    available_space_y = (ai_settings.screen_height -
+        (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
